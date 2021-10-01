4f93be4c6056c05dbc9c856e5ecf8df5572f50bc
Sebastian Pacurar <sebastianpacurar@users.noreply.github.com>
2021-10-02 02:37:10 +0300

    Improve layout (#2)
    * Added some margins to the buttons:
     - added some left and right margins to the bottom using layout.Insets()
     - added .gitignore file
    
    * Added a counter with increaser and decreaser:
      - planning to improve this specific counter since it's called multioption_counter.go
      - followed the official gioui example for counter to implement this way.
      - added an extra decrementing button.

---
+++ counters/multioption_counter.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/key"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+	"log"
+	"os"
+	"strconv"
+)
+
+var defaultMargin = unit.Dp(10)
+
+type C = layout.Context
+type D = layout.Dimensions
+
+func main() {
+	ui := NewUI()
+	go func() {
+		w := app.NewWindow(
+			app.Title("Title of the app"),
+			app.Size(unit.Dp(400), unit.Dp(600)),
+		)
+		if err := ui.Run(w); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+type counter struct {
+	count    int
+	increase widget.Clickable
+	decrease widget.Clickable
+}
+
+type UI struct {
+	theme   *material.Theme
+	counter counter
+}
+
+func (ui *UI) Run(w *app.Window) error {
+	var ops op.Ops
+	for event := range w.Events() {
+		switch event := event.(type) {
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, event)
+			ui.Layout(gtx)
+			event.Frame(gtx.Ops)
+		case key.Event:
+			switch event.Name {
+			case key.NameEscape:
+				return nil
+			}
+		case system.DestroyEvent:
+			return event.Err
+		}
+	}
+	return nil
+}
+
+func NewUI() *UI {
+	ui := &UI{}
+	ui.theme = material.NewTheme(gofont.Collection())
+	return ui
+}
+
+func (c *counter) Layout(th *material.Theme, gtx C) D {
+	return layout.Flex{
+		Axis: layout.Vertical,
+	}.Layout(
+		gtx,
+		layout.Flexed(1, func(gtx C) D {
+			return layout.Center.Layout(
+				gtx,
+				material.H2(th, strconv.Itoa(c.count)).Layout,
+			)
+		}),
+		layout.Rigid(
+			layout.Spacer{
+				Height: defaultMargin,
+			}.Layout,
+		),
+		layout.Flexed(2, func(gtx C) D {
+			for range c.increase.Clicks() {
+				c.count++
+			}
+			return material.Button(th, &c.increase, "Increase").Layout(gtx)
+		}),
+		layout.Rigid(
+			layout.Spacer{
+				Height: defaultMargin,
+			}.Layout,
+		),
+		layout.Flexed(2, func(gtx C) D {
+			for range c.decrease.Clicks() {
+				c.count--
+			}
+			return material.Button(th, &c.decrease, "Decrease").Layout(gtx)
+		}),
+	)
+}
+
+func (ui *UI) Layout(gtx C) D {
+	inset := layout.UniformInset(defaultMargin)
+	return inset.Layout(
+		gtx,
+		func(gtx C) D {
+			return ui.counter.Layout(ui.theme, gtx)
+		})
+}
