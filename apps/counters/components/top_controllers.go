f397f871b0154e3997d9566435f46fe470282e91
sebastianpacurar <pacurar.sebastian90@gmail.com>
2021-10-14 02:45:50 +0300

    create a validateTextField function to avoid redundancy for both value inputs:   - takes 4 params:     - a pointer to material.Theme     - a component.TextField     - a material.EditorStyle     - a pointer to a widget.Border

--- apps/counters/components/top_controllers.go
+++ apps/counters/components/top_controllers.go
@@ -26,35 +26,6 @@ func (vh *ValueHandler) InitTextFields() {
 	vh.unitVal.Alignment = layout.Alignment(text.Start)
 }
 
-// isFieldNumeric - verifies if the string can be converted to integer.
-// It iterates through the input and verifies if each rune digit casted to string,
-// can be converted to integer. Exception from the rule is the minus sign as first char.
-func isFieldNumeric(e component.TextField) bool {
-	if e.Len() > 0 {
-		for i := range e.Text() {
-			if i == 0 && e.Text()[0] == '-' {
-				continue
-			}
-			_, err := strconv.Atoi(string(e.Text()[i]))
-			if err != nil {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-// trimInput - removes the last rune from the string iteratively. Useful in case
-// the input char count passes 17 chars, and mostly In case the system breaks and
-// more than 18 characters can be provided.
-func trimInput(e *component.TextField, count int) {
-	str := e.Text()
-	for i := 0; i < count; i++ {
-		_, _ = utf8.DecodeLastRuneInString(str)
-	}
-	e.SetText(str[:len(str)-count])
-}
-
 // Layout - displays the Start From button and text input horizontally.
 // The editor is flexed, so it can enlarge/shrink while resizing on the X-Axis.
 func (vh *ValueHandler) Layout(th *material.Theme, gtx C) D {
@@ -92,23 +63,11 @@ func (vh *ValueHandler) Layout(th *material.Theme, gtx C) D {
 
 		// TextField Widget - to change start value
 		layout.Rigid(func(gtx C) D {
+			eStart.TextSize = unit.Sp(20)
+			eStart.HintColor = globals.Colours["dark-slate-grey"]
 			border := globals.DefaultBorder
+			validateTextField(th, vh.startVal, eStart, &border)
 
-			// 1) In case the length of the input is >= 18, then keep trimming chars
-			// until the length is back to 17
-			// 2) If there are non-numeric characters, change colors to red
-			// 3) When Focused change border color a bit
-			switch {
-			case vh.startVal.Len() >= 18:
-				trimInput(&vh.startVal, vh.startVal.Len()-18)
-			case isFieldNumeric(vh.startVal):
-				border.Color = globals.Colours["red"]
-				border.Width = unit.Px(5)
-				eStart.Color = globals.Colours["dark-red"]
-			case vh.startVal.Focused():
-				border.Color = th.Palette.ContrastBg
-				border.Width = unit.Px(3)
-			}
 			return border.Layout(gtx, func(gtx C) D {
 				return layout.UniformInset(unit.Dp(8)).Layout(
 					gtx,
@@ -136,23 +95,8 @@ func (vh *ValueHandler) Layout(th *material.Theme, gtx C) D {
 		layout.Rigid(func(gtx C) D {
 			eUnit.TextSize = unit.Sp(20)
 			eUnit.HintColor = globals.Colours["dark-slate-grey"]
-			border := widget.Border{
-				Color:        globals.Colours["grey"],
-				CornerRadius: unit.Dp(3),
-				Width:        unit.Px(2),
-			}
-
-			switch {
-			case vh.unitVal.Len() >= 18:
-				trimInput(&vh.unitVal, vh.unitVal.Len()-18)
-			case isFieldNumeric(vh.unitVal):
-				border.Color = globals.Colours["red"]
-				border.Width = unit.Px(5)
-				eStart.Color = globals.Colours["dark-red"]
-			case vh.unitVal.Focused():
-				border.Color = th.Palette.ContrastBg
-				border.Width = unit.Px(3)
-			}
+			border := globals.DefaultBorder
+			validateTextField(th, vh.startVal, eStart, &border)
 
 			return border.Layout(gtx, func(gtx C) D {
 				return layout.UniformInset(unit.Dp(8)).Layout(
@@ -162,4 +106,51 @@ func (vh *ValueHandler) Layout(th *material.Theme, gtx C) D {
 			})
 		}),
 	)
+}
+
+// isFieldNumeric - verifies if the string can be converted to integer.
+// It iterates through the input and verifies if each rune digit casted to string,
+// can be converted to integer. Exception from the rule is the minus sign as first char.
+func isFieldNumeric(e component.TextField) bool {
+	if e.Len() > 0 {
+		for i := range e.Text() {
+			if i == 0 && e.Text()[0] == '-' {
+				continue
+			}
+			_, err := strconv.Atoi(string(e.Text()[i]))
+			if err != nil {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// trimInput - removes the last rune from the string iteratively. Useful in case
+// the input char count passes 17 chars, and mostly In case the system breaks and
+// more than 18 characters can be provided.
+func trimInput(e component.TextField, count int) {
+	str := e.Text()
+	for i := 0; i < count; i++ {
+		_, _ = utf8.DecodeLastRuneInString(str)
+	}
+	e.SetText(str[:len(str)-count])
+}
+
+// 1) In case the length of the input is >= 18, then keep trimming chars
+// until the length is back to 17
+// 2) If there are non-numeric characters, change colors to red
+// 3) When Focused change border color a bit
+func validateTextField(th *material.Theme, e component.TextField, eStyle material.EditorStyle, b *widget.Border) {
+	switch {
+	case e.Len() >= 18:
+		trimInput(e, e.Len()-18)
+	case isFieldNumeric(e):
+		b.Color = globals.Colours["red"]
+		b.Width = unit.Px(5)
+		eStyle.Color = globals.Colours["dark-red"]
+	case e.Focused():
+		b.Color = th.Palette.ContrastBg
+		b.Width = unit.Px(3)
+	}
 }
