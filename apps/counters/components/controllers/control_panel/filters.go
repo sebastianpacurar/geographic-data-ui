35a8349ecb88a9471ad2b657805eb11a89e1967c
sebastianpacurar <pacurar.sebastian90@gmail.com>
2021-11-20 16:48:03 +0200

    Update Control Panel and add DisplayLayout and Filters UI controllers:   - Changed ControlPanel to handle all Controllers as ListElements, belonging to a vertical list     - The reason for this is to trigger vertical scrolling in case the content of all opened disclosers gets to large for the Constraints.Max.Y of the Control Panel layout     - Added a Controller struct which contains a string, name and a function, named layout     - ControlPanel struct contains all the layouts of the control_panel components, plus the discloser state for all of them, individually       - The reason for this is to keep track of jthe state separately, by isolating the DiscloserState components form each other     - Added a divider component which is always displayed below the current control_panel component     - Added a small LayOutset function to wrap the discloser and divider for all control_panel components   - Added UI for Dislpay Layout and Filters sections     - Display Layout refers to the way the displayedNumber(s) is/are displayed       - There are 3 options:         - Single Unit (default value), displays only the current number         - Grid, which will display the numbers as cards, containing relevant info about the current number           - This will make more sense after the Filters controller gets implemented         - Table, which will display the numbers in a tabular form, allowing the user to add/remove columns.           - Again, This will make sense after the Filters controller gets implemented. One Filter added = One Column added, and the other way around     - Filters refers to adding variations of the current number:       - Currently only 4 options were added:         - Ascii, which will convert the current number to ascii (if applicable, else it will be a null based element on the grid card, or table cell         - Binary, which will convert the current number to binary         - Octal, which will reflect the current's number octal value         - Hexadecimal, which will refer to the hexadecimal value of the current number   - Hardcoded the drawer modal, in case docking is enabled. It was set to 250 DP, since when on wider screens, the dynamic resize gets to be way too large   - Renamed several function from the data_handler to preserve encapsulation

---
+++ apps/counters/components/controllers/control_panel/filters.go
@@ -0,0 +1,33 @@
+package control_panel
+
+import (
+	"gioui.org/layout"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+type (
+	Filters struct {
+		ascii       widget.Bool
+		binary      widget.Bool
+		octal       widget.Bool
+		hexadecimal widget.Bool
+	}
+)
+
+func (f *Filters) Layout(gtx C, th *material.Theme) D {
+	return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+		layout.Rigid(func(gtx C) D {
+			return material.CheckBox(th, &f.ascii, "Ascii").Layout(gtx)
+		}),
+		layout.Rigid(func(gtx C) D {
+			return material.CheckBox(th, &f.binary, "Binary").Layout(gtx)
+		}),
+		layout.Rigid(func(gtx C) D {
+			return material.CheckBox(th, &f.octal, "Octal").Layout(gtx)
+		}),
+		layout.Rigid(func(gtx C) D {
+			return material.CheckBox(th, &f.hexadecimal, "Hexadecimal").Layout(gtx)
+		}),
+	)
+}
