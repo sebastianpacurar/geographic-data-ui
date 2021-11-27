2e75ffaffde640395aa1bbad551b8a2cdca232f9
sebastianpacurar <pacurar.sebastian90@gmail.com>
2021-11-27 16:11:13 +0200

    Fix BG Theme color issue:   - Whenever the Counter app was reached, and any widget was hovered, the contrast of the app was set to a lighter blue     - Fixed by removing the contrast.bg coloring on labeled_icon_widget.go       - This specific custom widget will be removed in the future anyway     - Removed the bgColor property from the LabeledIconBtn struct to prevent the issue from happening in the future     - Removed some gibberish comments since the functionality is obvious     - Some small fixes over the editor app       - Removed the active boolean from the TextArea struct, since it's being handled by the graphics context

--- custom_widgets/labeled_icon_widget.go
+++ custom_widgets/labeled_icon_widget.go
@@ -10,35 +10,26 @@ import (
 	"image/color"
 )
 
-// LabeledIconBtn - component which returns a custom-made widget, based on its given properties
 type LabeledIconBtn struct {
-	Theme               *material.Theme
-	BgColor, LabelColor color.NRGBA
-	Button              *widget.Clickable
-	Icon                *widget.Icon
-	Label               string
+	Theme      *material.Theme
+	LabelColor color.NRGBA
+	Button     *widget.Clickable
+	Icon       *widget.Icon
+	Label      string
 }
 
-// Layout - currently returns a button-like widget, which contains:
-// an icon on the left side and a text label n the right side.
-// These 2 are separated by 5 device pixels
 func (lib LabeledIconBtn) Layout(gtx layout.Context) layout.Dimensions {
-
-	// Set custom colours for Icon and Label
 	// Set the TextSize for the Label
-	lib.Theme.Palette.ContrastBg = lib.BgColor
 	lib.Theme.TextSize.Scale(14.0 / 16.0)
 
 	// return a ButtonLayout dimension which contains the Icon and Label
 	return material.ButtonLayout(lib.Theme, lib.Button).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 		return layout.UniformInset(globals.DefaultMargin).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 
-			// labeledIcon will be used to return all its content dimensions
 			labeledIcon := layout.Flex{
 				Axis:      layout.Horizontal,
 				Alignment: layout.Middle,
 			}
-			// spacer is used for separating the Icon from the Label by an amount of pixels
 			spacer := unit.Dp(5)
 			// This is the actual Icon of the Button. It will return the dimensions of the Icon widget
 			layIcon := layout.Rigid(func(gtx layout.Context) layout.Dimensions {
