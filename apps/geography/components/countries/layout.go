0e7d3bbf330a36b64622f07bc06cfaa6dc980e67
sebastianpacurar <pacurar.sebastian90@gmail.com>
2021-12-26 15:38:02 +0200

    some code refactoring on Geo app

--- apps/geography/components/countries/layout.go
+++ apps/geography/components/countries/layout.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"gioui-experiment/apps/geography/components/countries/data"
 	"gioui-experiment/apps/geography/components/countries/grid"
+	"gioui-experiment/apps/geography/components/countries/table"
 	g "gioui-experiment/globals"
-	"gioui-experiment/themes/colors"
 	"gioui.org/layout"
-	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"gioui.org/x/outlay"
-	"image"
 )
 
 type (
@@ -21,69 +19,14 @@ type (
 
 type (
 	Display struct {
+		grid.Card
 		cards []grid.Card
 		data.Countries
-		grid outlay.GridWrap
-		list widget.List
+		grid  outlay.GridWrap
+		list  widget.List
+		table table.Table
 	}
 )
-
-func (d *Display) LayCard(gtx C, th *material.Theme, card *grid.Card) D {
-	size := image.Pt(150, 200)
-	gtx.Constraints = layout.Exact(gtx.Constraints.Constrain(size))
-	return layout.Stack{}.Layout(gtx,
-		layout.Expanded(func(gtx C) D {
-			return widget.Border{
-				Color:        g.Colours[colors.GREY],
-				CornerRadius: unit.Dp(18),
-				Width:        unit.Px(2),
-			}.Layout(gtx, func(gtx C) D {
-				area := material.Clickable(gtx, &card.Click, func(gtx C) D {
-
-					if card.Click.Clicked() {
-						fmt.Println(fmt.Sprintf("%s", card.Name))
-					}
-
-					return g.RColoredArea(gtx,
-						size,
-						unit.Dp(18),
-						g.Colours[colors.WHITE],
-					)
-				})
-				return area
-			})
-		}),
-		layout.Stacked(func(gtx C) D {
-			return g.Inset.Layout(gtx, func(gtx C) D {
-				gtx.Constraints = layout.Exact(gtx.Constraints.Constrain(size))
-				return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceAround}.Layout(gtx,
-
-					// country name
-					layout.Rigid(func(gtx C) D {
-						return layout.Flex{}.Layout(gtx,
-							layout.Flexed(1, func(gtx C) D {
-								return layout.Center.Layout(gtx, func(gtx C) D {
-									return material.Body2(th, card.Name).Layout(gtx)
-								})
-							}),
-						)
-					}),
-
-					// TODO: fix this or find a workaround
-					// (capital area) country flag (temporary broken)
-					layout.Rigid(func(gtx C) D {
-						//country.flag = d.processFlagFromURL(country)
-						return layout.Flex{}.Layout(gtx,
-							layout.Flexed(1, func(gtx C) D {
-								return layout.Center.Layout(gtx, func(gtx C) D {
-									return material.Body2(th, card.Capital).Layout(gtx)
-								})
-							}))
-					}))
-			})
-		}))
-
-}
 
 func (d *Display) Layout(gtx C, th *material.Theme) D {
 	err := d.InitCountries()
---
+++ apps/geography/components/countries/table/table.go
@@ -0,0 +1,16 @@
+package table
+
+import "gioui.org/layout"
+
+type (
+	C = layout.Context
+	D = layout.Dimensions
+
+	Table struct {
+	}
+)
+
+// Layout -  TODO: Disbaled - in progress
+func (t *Table) Layout(gtx C) D {
+	return D{}
+}
